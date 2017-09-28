d2d2daeff6c588e107a1711014c72489126d51b4
Danny Nielsen <danny.nlsn0@gmail.com>
2017-09-28 15:10:55 +0200

    Subset handling.

--- src/com/group14/DifferenceSet.java
+++ src/com/group14/DifferenceSet.java
@@ -2,19 +2,20 @@ package com.group14;
 
 import java.util.Arrays;
 
-public class DifferenceSet {
-    private Object[] A;
-    private Object[] B;
+public class DifferenceSet<T> implements Set<T> {
+    private T[] A;
+    private T[] B;
 
-    public DifferenceSet(Object[] A, Object[] B){
+    public DifferenceSet(T[] A, T[] B){
         this.A = A;
         this.B = B;
     }
 
-    public boolean difference(Object o) {
-        if(Arrays.asList(A).contains(o) && !Arrays.asList(B).contains(o)) return true;
-        else if(!Arrays.asList(A).contains(o) && Arrays.asList(B).contains(o)) return true;
-        return false;
 
+    @Override
+    public boolean contains(T element) {
+        if(Arrays.asList(A).contains(element) && !Arrays.asList(B).contains(element)) return true;
+        else if(!Arrays.asList(A).contains(element) && Arrays.asList(B).contains(element)) return true;
+        return false;
     }
 }
--- src/com/group14/Main.java
+++ src/com/group14/Main.java
@@ -1,8 +1,12 @@
 package com.group14;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        //Set handling
+
         String[] members = {"Danny", "Kim", "Alexander"};
         MemberShipSet memberShipSet = new MemberShipSet(members);
 
@@ -19,13 +23,42 @@ public class Main {
         System.out.println("Union: " + unionSet.contains("BMW"));
 
         DifferenceSet differenceSet = new DifferenceSet(coolCars, affordableCars);
-        System.out.println("Differences: " +  differenceSet.difference("Audi"));
+        System.out.println("Differs: " + differenceSet.contains("Audi"));
 
 
         ComplementSet complementSet = new ComplementSet(members);
         System.out.println("Complement: " + complementSet.contains("Mathias"));
 
 
+        //-----------------------------------------------------------------------
+        //Subset handling
+        String[] a = {"2", "3"};
+        String[] b = {"1", "2", "3", "4"};
+
+        System.out.println("Subset resultcode: " + determineSubset(a, b));
+        System.out.println("Equals resultcode: " + determineEquals(a, b));
+        System.out.println("Superset resultcode: " + determineSuperset(b,a));
 
     }
+
+    private static int determineSubset(String[] a, String[] b) {
+        for (String element : b) {
+            if (Arrays.asList(a).contains(element)) return -1;
+        }
+        return -2;
+    }
+
+    private static int determineEquals(String[] a, String[] b) {
+        if (Arrays.equals(a, b)) return 0;
+        return -2;
+    }
+
+    private static int determineSuperset(String[] a, String[] b){
+        if(Arrays.asList(a).containsAll(Arrays.asList(b)) && a.length > b.length) return 1;
+        return -2;
+    }
+
+
+
+
 }
