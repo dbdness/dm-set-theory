a33054e9aa3b8abf7c9b72586d19b35769dfc979
KIMB0 <Kimsen1992@gmail.com>
2017-09-29 10:53:49 +0200

    Added intersection function.

--- JavaScript/Sets/sets.js
+++ JavaScript/Sets/sets.js
@@ -1,13 +1,34 @@
+// ABOUT -- This is a exapmle of sets in JavaScript.
+
 var car = ["wheels", "brakes", "motor", ]
 var bicycle = ["wheels", "brakes", "saddle"]
 
 var setA = new Set(car);
 var setB = new Set(bicycle);
 
+// === MEMBERSHIP ===
 // Here we are just checking if setA has "saddle".
 console.log(setA.has("saddle")); // Should return 'false'.
 
-// UNION FUNCTION: 
+
+
+// === INTERSECTION ===
+// Here we are saying, if a given set has the element of anySet, then add it to a new set called 'intersection' which we return.
+Set.prototype.intersection = function(anySet){
+    var intersection = new Set();
+    for(var element of anySet){
+        if (this.has(element)) {
+            intersection.add(element);
+        }
+    }
+    return intersection;
+}
+
+console.log("Intersection:", setB.intersection(setA)); // Should return 'Intersection: Set { 'wheels', 'brakes' }'.
+
+
+
+// === UNION FUNCTION === 
 // Here we create a function that puts an element of anySet on a given set.
 Set.prototype.union = function (anySet) {
     var union = new Set(this);
@@ -18,4 +39,5 @@ Set.prototype.union = function (anySet) {
 }
 
 console.log("Union", setA.union(setB)); // Should return 'Union Set { 'wheels', 'brakes', 'motor', 'saddle' }'
+
 
